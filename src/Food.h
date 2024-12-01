13eb4f76d8961ff2b5dd0dd6db3b3adb112a2188
Weston Trefry <westontrefry@Westons-MacBook-Pro.local>
2024-11-30 22:20:28 -0700

    added attributes in Food class

--- src/Food.h
+++ src/Food.h
@@ -4,10 +4,29 @@
 
 #ifndef FOOD_H
 #define FOOD_H
-
+#include <string>
 
 
 class Food {
+public:
+    /*=== Constructors ===*/
+
+    /*=== Getter Functions ===*/
+private:
+    /*=== Food Attributes ===*/
+    std::string description;
+    float _fiber = 0.0f;
+    float _protein = 0.0f;
+    float _sodium = 0.0f;
+    float _sugars = 0.0f;
+    float _satFats = 0.0f;
+    float _energy = 0.0f;
+    // Count to keep track of the number of food objects instantiated
+    unsigned int count = 0;
+
+    /*=== File Functions (for CSV) ===*/
+
+    /*=== Search Functions ===*/
 
 };
 
