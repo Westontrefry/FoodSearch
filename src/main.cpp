e4cbf22adf393b0c0c282102883bfaaecacb2bad
Weston Trefry <westontrefry@Westons-MBP.lan>
2024-12-01 00:26:08 -0700

    feat: declared constructors, getters, readFile function in Food.h, skeleton comments in main.cpp for main logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,38 @@
 //
 // Created by Weston Trefry on 11/30/24.
 //
+#include <iostream>
+#include "Food.h"
+
+int main() {
+    /*=== Instantiate Data Structure Objects ===*/
+    // Create Table Object
+    // Create Graph Object
+
+    /*=== Load Data from FoodData.csv ===*/
+    // Read file using Food::readFile function and pushback onto vector
+    // Iterate over vector and build both data structures item by item
+
+    /*=== Main logic loop ===*/
+    // Prompt user for input (search a food item)
+    // Prompt user to select a filter (menu of nutrient types)
+    // Give user option to exit the program (exit while loop)
+
+        /*=== Building Data Structures + Search ===*/
+        // Ask user what data structure to build: 1. HashTable, 2. Graph
+        // If hash table
+            // Get items using hashtable search function
+            // Push back onto vector
+        // If graph
+            // Get items using graph search function
+            // Push back onto vector
+
+        /*=== Displaying Search Results ===*/
+        // Display vector items to console
+        // Write items to a CSV (in a pair)
+
+
+
+
+    return 0;
+}
