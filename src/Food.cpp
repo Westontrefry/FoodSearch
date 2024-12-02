aeff816ba93fd6ba9c46ab4c6f511e5519e0cad8
ShanonB <92969175+ShanonB@users.noreply.github.com>
2024-12-02 14:25:36 -0500

    Update Food.cpp

--- src/Food.cpp
+++ src/Food.cpp
@@ -3,6 +3,64 @@
 //
 
 #include "Food.h"
+#include <fstream>
+
+
+// addded ShanonB
+
+unsigned int Food::count = 0;
+
+Food::Food(std::string name, float fiber, float protein, float sodium,     
+           float sugars, float satFats, float energy)
+    : description(std::move(name)), fiber(fiber), protein(protein),
+      sodium(sodium), sugars(sugars), satFats(satFats), energy(energy)
+{
+    ++count; 
+}
+
+std::string Food::getDescription() const
+{
+    return description;
+}
+
+float Food::getFiber() const
+{
+    return fiber;
+}
+
+float Food::getProtein() const
+{
+    return protein;
+}
+
+float Food::getSodium() const
+{
+    return sodium;
+}
+
+float Food::getSugars() const
+{
+    return sugars;
+}
+
+float Food::getSatFats() const
+{
+    return satFats;
+}
+
+float Food::getEnergy() const
+{
+    return energy;
+}
+
+unsigned int Food::getCount()
+{
+    return count;
+}
+
+/// ***** added ShanonB
+
+
 
 // Read File implementation
 std::vector<Food*> readFile(const std::string& fileName) {
