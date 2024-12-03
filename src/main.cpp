d280f37052aaa18d19c01a1f0207ec70f3e4d5a4
ShanonB <92969175+ShanonB@users.noreply.github.com>
2024-12-03 01:09:30 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 // Created by Weston Trefry on 11/30/24.
 //
 #include <iostream>
+#include <string>
 #include "Food.h"
 #include "FoodGraph.h" //add header file - ShanonB
 #include "FoodHashTable.h"
@@ -32,16 +33,132 @@ int main() {
         // Prompt user for input (search a food item)
         std::cout << "Select 1 to search for a food item." << std::endl;
         std::cout << "Select 2 to exit the program." << std::endl;
-        std::string userInput;
+        int userInput;                                                    //changed to int - ShanonB
+        std::cin >> userInput;                                            // added ShanonB
 
         if (userInput == "2") {
             break;
         }
-        if (userInput == "1") {
+      //  if (userInput == "1") {
             // Prompt user to select a filter (menu of nutrient types)
-            std::cout << "Filter by nutrient: " << std::endl;
-            std::cout << "1. Fiber\n 2. Protein \n 3. Sodium \n "
-                         "4. Sugars \n 5. Saturated Fats \n";
+     ///       std::cout << "Filter by nutrient: " << std::endl;
+      //      std::cout << "1. Fiber\n 2. Protein \n 3. Sodium \n "
+      //                   "4. Sugars \n 5. Saturated Fats \n";
+      //  }
+
+        if (userInput == 1) {
+            // Prompt user to select a filter (menu of nutrient types)    -- modified ShanonB
+            std::cout << "Search for food name or by food description keyword or filter by nutrient: " << std::endl;
+            std::cout << "1. Food Name\n2. Food Description Keyword\n"
+                         "3. Fiber\n4. Protein \n5. Sodium \n"
+                         "6. Sugars \n7. Saturated Fats \n8. Exit Program";
+            int filterChoice;
+            std::cout << "Enter the nutrient number: ";
+            std::cin >> filterChoice;                           //added ShanonB
+            
+            if (filterChoice == 8)
+            {
+                break;
+            }
+
+            int tableType;
+            std::cout << "Select the Type of Data Structure to use.\n";
+            std::cout << "  Select 1 to use the HashTable.\n";
+            std::cout << "  Select 2 to use the Graph.\n";
+            std::cout << "  Select 3 to exit the program.\n";
+            std::cin >> tableType;
+
+
+            if (tableType == 1)
+            {
+                std::cout << "You have chosen the HashTable." << std::endl;
+                switch (filterChoice)
+                {
+                case 1:
+                    // add hash function
+                        continue;
+                case 2:
+                    // add hash function
+                        continue;
+                case 3:
+                    // add hash function
+                        continue;
+                case 4:
+                    // add hash function
+                        continue;
+                case 5:
+                    // add hash function
+                        continue;
+                }
+            }
+            else if (tableType == 2)
+            {
+                std::cout << "You have chosen the Graph." << std::endl;
+                switch (filterChoice)
+                {
+                case 1:
+                    {
+                        std::string foodName;
+                        std::cout << "Search by food name, enter the name: ";
+                        std::cin >> foodName;
+                        foodGraph.search_by_name(foodName);
+                        continue;
+                    }
+                case 2:
+                    {
+                        std::string keyword;
+                        std::cout << "Search by food description keyword (ex. cracker), enter the keyword: ";
+                        std::cin >> keyword;
+                        foodGraph.search_by_description_bfs(keyword);
+                        continue;
+                    }
+                case 3:
+                    {
+                        float fiberAmount;
+                        std::cout << "Enter the amount of fiber to search for: ";
+                        std::cin >> fiberAmount;
+                        foodGraph.search_by_fiber(fiberAmount);
+                        continue;
+                    }
+                case 4:
+                    {
+                        float proteinAmount;
+                        std::cout << "Enter the amount of protein to search for: ";
+                        std::cin >> proteinAmount;
+                        foodGraph.search_by_protein(proteinAmount);
+                        continue;
+                    }
+                case 5:
+                    {
+                        float sodiumAmount;
+                        std::cout << "Enter the amount of sodium to search for: ";
+                        std::cin >> sodiumAmount;
+                        foodGraph.search_by_sodium(sodiumAmount);
+                        continue;
+                    }
+                case 6:
+                    {
+                        float sugarsAmount;
+                        std::cout << "Enter the amount of sugars to search for: ";
+                        std::cin >> sugarsAmount;
+                        foodGraph.search_by_sugars(sugarsAmount);
+                        continue;
+                    }
+                case 7:
+                    {
+                        float fatsAmount;
+                        std::cout << "Enter the amount of saturated fats to search for: ";
+                        std::cin >> fatsAmount;
+                        foodGraph.search_by_saturated_fat(fatsAmount);
+                        continue;
+                    }
+
+                }
+            }
+            else if (tableType == 3)
+            {
+                break;
+            }
         }
 
         // Give user option to exit the program (exit while loop)
